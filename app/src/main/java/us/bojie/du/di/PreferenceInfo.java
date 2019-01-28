2a2e9f5f06010783eeab857c25203b558ca86963
Bojie Jiang <jiangbojie@gmail.com>
2019-01-28 08:50:11 -0800

    PreferenceInfo

---
+++ app/src/main/java/us/bojie/du/di/PreferenceInfo.java
@@ -0,0 +1,12 @@
+package us.bojie.du.di;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PreferenceInfo {
+
+}
